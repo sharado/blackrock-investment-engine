d2e98a455f73cbe8f1b67386e0368aa81a3e35e2
sharado <ozashrd@gmail.com>
2026-02-21 16:28:16 +0530

    Bootstrap Spring Boot project (Java 21)

---
+++ src/main/java/com/blackrock/challenge/investmentengine/BlackrockApplication.java
@@ -0,0 +1,13 @@
+package com.blackrock.challenge.investmentengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackrockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackrockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackrock/challenge/investmentengine/BlackrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackrock.challenge.investmentengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackrockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
